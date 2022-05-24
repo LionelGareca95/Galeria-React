65ab3f0b3ff63274d58ec04f9abf8dc48a2285ac
LionelGareca95 <lionelgareca@gmail.com>
2022-05-24 16:37:32 -0300

    segundo commit

---
+++ src/components/services/services.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const services = () => {
+  return (
+    <div className="services">
+        {/* Left Side */}
+        <div className="awesome">
+
+        </div>
+        {/* Right Side */}
+        <div className="cards"></div>
+    </div>
+  )
+}
+
+export default services
