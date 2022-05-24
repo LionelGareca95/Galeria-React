d281aae599a6cd5442abb44ff6815a96c7cd5a5f
LionelGareca95 <lionelgareca@gmail.com>
2022-05-24 17:12:11 -0300

    Nuevos Cambios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar/Navbar";
 import './App.css'
 import Intro from "./components/Navbar/Intro/Intro";
+import Services from "./components/services/Services";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Intro/>
+      <Services/>
     </div>
   );
 }
--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
+import './Services.css'
+import Glasses from '../../img/glasses.png'
+import HeartEmoji from '../../img/heartemoji.png'
+import Humble from '../../img/humble.png'
 
-const services = () => {
+const Services = () => {
   return (
     <div className="services">
-        {/* Left Side */}
-        <div className="awesome">
-
-        </div>
-        {/* Right Side */}
-        <div className="cards"></div>
+    {/* Left Side */}
+    <div className="awesome">
+      <span>My Awesome</span>
+      <span>Services</span>
+      <span> Commodi provident ea earum obcaecati, aliquam, id necessitatibus nihil et inventore distinctio iure fuga, unde veniam expedita sapiente quia quas quisquam eaque!</span>
+      <br/>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+      <button className='button s-button'>Download CV</button>
+      <div className='blur s-blur' style={{ background: "#ABF1FF94" }}></div>
     </div>
+    {/* Right Side */}
+    <div className="cards">
+      I am de Right Side
+    </div>
+</div>
   )
 }
 
-export default services
+export default Services
